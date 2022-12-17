5232bf29439e7fbee62d9ac5d3b3fc0eab1a5794
siddikgitt <siddikkhanpathan4@gmail.com>
2022-12-17 14:11:07 +0530

    done

---
+++ Controller/job.router.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const Job = require("../Models/Job.model");
+const jobRouter = express.Router();
+jobRouter.post("/", async (req, res) => {
+  try {
+    const data = await Job.create(req.body);
+    return res.status(200).send({ message: "Data added", data: data });
+  } catch (e) {
+    return res.status(500).send("Server Error");
+  }
+});
+
+jobRouter.get("/", async (req, res) => {
+  const { sort, search, filter, page = 1, limit = 10 } = req.query;
+  try {
+    let newJob;
+    let len = await Job.find()
+    if (search) {
+      if (filter) {
+        if (sort == "asc") {
+          newJob = await Job.find({ role: filter })
+            .sort({ createdAt: 1 })
+            .skip(10 * (page - 1))
+            .limit(limit);
+        } else if (sort == "desc") {
+          newJob = await Job.find({ role: filter })
+            .sort({ createdAt: -1 })
+            .skip(10 * (page - 1))
+            .limit(limit);
+        }
+      } else {
+        if (sort == "asc") {
+          newJob = await Job.find({ language: search })
+            .sort({ createdAt: 1 })
+            .skip(10 * (page - 1))
+            .limit(limit);
+        } else if (sort == "desc") {
+          newJob = await Job.find({ language: search })
+            .sort({ createdAt: -1 })
+            .skip(10 * (page - 1))
+            .limit(limit);
+        }
+      }
+    } else if (filter) {
+      if (sort == "asc") {
+        newJob = await Job.find({ role: filter })
+          .sort({ createdAt: 1 })
+          .skip(10 * (page - 1))
+          .limit(limit);
+      } else if (sort == "desc") {
+        newJob = await Job.find({ role: filter })
+          .sort({ createdAt: -1 })
+          .skip(10 * (page - 1))
+          .limit(limit);
+      }
+    } else {
+      if (sort == "asc") {
+        newJob = await Job.find()
+          .sort({ createdAt: 1 })
+          .skip(10 * (page - 1))
+          .limit(limit);
+      } else if (sort == "desc") {
+        newJob = await Job.find()
+          .sort({ createdAt: -1 })
+          .skip(10 * (page - 1))
+          .limit(limit);
+      }
+    }
+    
+    return res.status(200).send({ message: "Product get successfully", data: newJob, totalPages: Math.ceil(len.length / limit) })
+
+  } catch (e) {
+    return res.status(500).send("Internal server error");
+  }
+});
+
+module.exports = jobRouter;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const connect = require("../Connect/Connect");
 const req = require("express/lib/request");
-const List = require("../Models/list.model");
+const jobRouter = require("../Controller/job.router");
 const PORT = process.env.PORT || 8080;
 mongoose.set("strictQuery", true);
 const server = express();
@@ -14,46 +14,8 @@ server.get("/", async (req, res) => {
   res.status(200).send("Hello Welcome to my server");
 });
 
-// GET LIST
-server.get("/list", async (req, res) => {
-  try {
-    const data = await List.find();
-    res.send({ data: data });
-  } catch (error) {
-    res.status(501).send(error.message);
-  }
-});
-
-// LIST ADD
-server.post("/list", async (req, res) => {
-  let dateandtimestamp = new Date().toLocaleString("en-US", {
-    timeZone: "Asia/Kolkata"
-  });
-  let list = { ...req.body, dateandtimestamp: dateandtimestamp };
-  console.log(list);
-  try {
-    const data = await List.create(list);
-    res.send({ message: "List added successfully", list });
-  } catch (error) {
-    res.status(501).send(error.message);
-  }
-});
-
-// LIST DELETE
-server.delete("/list/:id", async (req, res) => {
-  let id = req.params.id;
-  try {
-    id = id.toString();
-    let data = await List.findByIdAndDelete(id);
-    if (!data) {
-      res.status(401).send("data not found");
-    } else {
-      res.send("List Deleted Successfully");
-    }
-  } catch (error) {
-    res.status(401).send(error.message);
-  }
-});
+//JOB ROUTER
+server.use("/job", jobRouter);
 
 server.listen(PORT, async () => {
   await connect();
